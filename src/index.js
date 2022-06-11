d412e712276a182ada17fd6830bd71d1daf9f413
Sota <30281161+sota0121@users.noreply.github.com>
2022-06-11 21:23:05 +0900

    sckelton complete btn remove btn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,39 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  alert();
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // -------------------------------------
+  // Compose todo item
+  // -------------------------------------
+  // container
+  const div = document.createElement("div");
+  div.className = "list-row";
+  // todo label
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  // complete button
+  const completeBtn = document.createElement("button");
+  completeBtn.innerText = "complete";
+  completeBtn.addEventListener("click", () => {
+    alert("complete");
+  });
+
+  // remove button
+  const removeBtn = document.createElement("button");
+  removeBtn.innerText = "remove";
+  removeBtn.addEventListener("click", () => {
+    alert("remove");
+  });
+
+  // compose div(list-row)
+  div.appendChild(li);
+  div.appendChild(completeBtn);
+  div.appendChild(removeBtn);
+
+  // add to todo-list
+  document.getElementById("todo-list").appendChild(div);
 };
 
 document
